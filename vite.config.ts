3be2357e08b84ea6152eae8cfb4ab4d900ef0588
henzyd <henzydee@gmail.com>
2024-02-18 02:25:06 +0100

    setup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import path from "path";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      port: parseInt(process.env.VITE_PORT) || 5173,
+      host: true,
+    },
+  });
+};
